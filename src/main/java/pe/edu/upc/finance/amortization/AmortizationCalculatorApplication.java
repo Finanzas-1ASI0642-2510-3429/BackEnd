0c24f8877595708168b27e11536df055a389d19c
Miguel26112001 <u202220294@upc.edu.pe>
2025-06-16 15:21:22 -0500

    chore: Base project addition

---
+++ src/main/java/pe/edu/upc/finance/amortization/AmortizationCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.finance.amortization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmortizationCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmortizationCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/finance/amortization/AmortizationCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.finance.amortization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmortizationCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
